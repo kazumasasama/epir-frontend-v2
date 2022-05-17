18733b5e0d4fdff49dbef8a55db10fa4ae513f61
Kaz <takaik@yahoo.com>
2022-05-17 15:25:14 -0400

    improve axios baseURL

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://frozen-wave-84233.herokuapp.com" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "production" ? "https://frozen-wave-84233.herokuapp.com" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
