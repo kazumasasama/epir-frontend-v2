2a4b81ab4b22324f89fdbb64acd1760d5692a86c
Kaz <takaik@yahoo.com>
2022-08-07 20:41:15 -0400

    translate Home page to English

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,12 @@
   <div class="container home-container">
      <div class="row">
        <div class="col-sm-12">
-        <h5 class="card-title top-card-title">脱毛サロン エピア祖師谷 epiR</h5>
-        <h6 class="card-title top-card-title">予約サイト</h6>
+        <h4 class="card-title top-card-title">Welcome to Booking App Biznesu</h4>
         <div class="card-body home" id="card-home">
           <img src="@/assets/home-illustration.png" class="card-img-home">
-          <p class="card-text" id="text-home">ご利用には登録・ログインが必要です</p>
-          <a class="btn btn-primary" href="/login">ログイン</a>
-          <a href="/signup" class="btn btn-primary">登録して予約</a>
+          <p class="card-text" id="text-home">Please login/signup to book an appontment</p>
+          <a class="btn btn-primary" href="/login">Login</a>
+          <a href="/signup" class="btn btn-primary">Signup</a>
           <!-- <p>or</p>
           <a href="/appointments" class="btn btn-primary">Continue as guest</a> -->
         </div>
