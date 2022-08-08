34fe9e72996cd58155456af13fba98970c9fd20a
Kaz <takaik@yahoo.com>
2022-08-08 15:08:08 -0400

    improve top page, change top illustration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="container home-container">
-     <div class="row">
-       <div class="col-sm-12">
+    <div class="row">
+      <div class="col-sm-12">
         <h4 class="card-title top-card-title">Welcome to Booking App Biznesu</h4>
         <div class="card-body home" id="card-home">
-          <img src="@/assets/home-illustration.png" class="card-img-home">
+          <img src="@/assets/people-top-image.jpg" class="card-img-home">
+          <p class="illust-credit">
+            <a href="https://www.freepik.com/vectors/people-set">People set vector created by pikisuperstar - www.freepik.com</a>
+          </p>
           <p class="card-text" id="text-home">Please login/signup to book an appontment</p>
-          <a class="btn btn-primary" href="/login">Login</a>
-          <a href="/signup" class="btn btn-primary">Signup</a>
           <!-- <p>or</p>
           <a href="/appointments" class="btn btn-primary">Continue as guest</a> -->
         </div>
-        <div class="col-sm-12 login-hint">
-          <p class="login-hint-title">Login as Admin</p>
-          <p class="login-hint-item"><small>Email: test@test.com</small></p>
-          <p class="login-hint-item"><small>Password: password</small></p>
-        </div>
-        <div class="col-sm-12 login-hint">
-          <p class="login-hint-title">Login as User</p>
-          <p class="login-hint-item"><small>Email: user@user.com</small></p>
-          <p class="login-hint-item"><small>Password: password</small></p>
-        </div>
+      </div>
+      <div class="col-sm-6 login-hint">
+        <p class="login-hint-title">Login as Admin</p>
+        <p class="login-hint-item"><small>Email: test@test.com</small></p>
+        <p class="login-hint-item"><small>Password: password</small></p>
+      </div>
+      <div class="col-sm-6 login-hint">
+        <p class="login-hint-title">Login as User</p>
+        <p class="login-hint-item"><small>Email: user@user.com</small></p>
+        <p class="login-hint-item"><small>Password: password</small></p>
+      </div>
+      <div class="col-12">
+        <a class="btn btn-primary" href="/login">Login</a>
+        <a href="/signup" class="btn btn-primary">Signup</a>
       </div>
     </div>
   </div>
@@ -43,9 +48,6 @@ export default {
 .btn {
   margin: 5px 8px;
 }
-#text-home {
-  max-width: 400px;
-}
 .home-container {
   display: flex;
   justify-content: center;
@@ -57,5 +59,8 @@ export default {
 }
 .login-hint-title {
   font-weight: bold;
+}
+.illust-credit {
+  font-size: 7pt;
 }
 </style>
